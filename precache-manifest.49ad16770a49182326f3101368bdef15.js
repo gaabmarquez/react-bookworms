7948ad193fab1433882c74d4b75e1a46923c0bbf
Gabriel Marquez <gaabmarquez@gmail.com>
2020-02-10 10:32:38 -0300

    Updates

---
+++ precache-manifest.49ad16770a49182326f3101368bdef15.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0104e8fda34f59bf6ba5b2810e4e3f62",
+    "url": "/react-bookworms/index.html"
+  },
+  {
+    "revision": "f92a6046c33c5e5dd0d8",
+    "url": "/react-bookworms/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "49836d87caf048218b44",
+    "url": "/react-bookworms/static/css/main.10b6263a.chunk.css"
+  },
+  {
+    "revision": "f92a6046c33c5e5dd0d8",
+    "url": "/react-bookworms/static/js/2.a7049d39.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-bookworms/static/js/2.a7049d39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49836d87caf048218b44",
+    "url": "/react-bookworms/static/js/main.ceed5f03.chunk.js"
+  },
+  {
+    "revision": "11846e11d86d5ab27078",
+    "url": "/react-bookworms/static/js/runtime-main.a5225e4e.js"
+  },
+  {
+    "revision": "e5bea38eef84df391cf2763c41f5a608",
+    "url": "/react-bookworms/static/media/landing-bg.e5bea38e.jpg"
+  }
+]);
